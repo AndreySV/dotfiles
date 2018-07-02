d02c33ad5ccd65b7f417e4bb8466739e8943000b
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2018-07-02 17:31:33 +0300

    remove installed emacs packages from the repo
    since version 25, selected packages can be installed using command "package-install-selected-packages"

--- emacs.d/elpa/find-file-in-git-repo-0.1.2/find-file-in-git-repo-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; find-file-in-git-repo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("find-file-in-git-repo.el") (21098 11565
-;;;;;;  981641 453000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; find-file-in-git-repo-autoloads.el ends here
